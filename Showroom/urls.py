c1dfd4cbe9f15c28970b77c378ce3cd88bfef0e4
zeeshan07github <saloo9615@gmail.com>
2023-08-08 15:34:04 +0500

    updated second commit

---
+++ Showroom/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path 
+from . import views
+
+app_name = 'Showroom'
+urlpatterns = [
+    path("" , views.brand_list , name ='brand_list'),
+    path("<int:profile_id>" , views.brand_detail , name ='brand_detail'),
+    path("<int:profile_id>/delete/" , views.brand_delete , name ='brand_delete'),
+    path("team/" , views.our_team , name ='our_team'),
+    path("Showrooms.html/" , views.showroomss , name ='showroomss'),
+    path("contactus/" , views.contactview , name ='contactus'),
+    path("newprofile/" , views.Newprofile , name ='newprofile'),
+    path("newmodel/" , views.Newmodel , name ='newmodel'),
+    path("filter/" , views.filters , name ='filter'),
+
+]
